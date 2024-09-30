46d4c9f983bed9219a2cf897595979241b9c7117
Yan Wallas <yanwallas98@gmail.com>
2024-09-30 18:13:21 -0300

    Divisão

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,9 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Soma();
+            //Soma();
+            //Subtracao();
+            Divisao();
         }
 
         static void Soma()
@@ -21,7 +23,40 @@ namespace Calculator
             Console.WriteLine("");
 
             float resultado = v1 + v2;
-            Console.WriteLine($"O resultado é {resultado}");
+            Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Subtracao()
+        {
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.ReadKey();
         }
     }
 }
