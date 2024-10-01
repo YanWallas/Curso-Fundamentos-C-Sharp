6583b1121ff0dab7e81b3a5063d68c33c4b5b29c
Yan Wallas <yanwallas98@gmail.com>
2024-10-01 18:10:21 -0300

    Sair do switch

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -23,6 +23,7 @@ namespace Calculator
             Console.WriteLine("2 - Subtração");
             Console.WriteLine("3 - Divisão");
             Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
 
             Console.WriteLine("------------------");
             Console.WriteLine("Selecione uma opção: ");
@@ -34,6 +35,7 @@ namespace Calculator
                 case 2: Subtracao(); break;
                 case 3: Divisao(); break;
                 case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;//sair da aplicação.
                 default: Menu(); break;
             }
         }
