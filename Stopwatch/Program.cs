17eb781b7ca223d1e0ba70320c3625047c5bf188
Yan Wallas <yanwallas98@gmail.com>
2024-10-02 16:46:14 -0300

    pegando ultimo caracter

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -7,7 +7,23 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Start(6);
+            //Start(6);
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S - segundos => 10s = 10 Segundos");
+            Console.WriteLine("M = Minutos => 1m = 1 Minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();//ToLower deixa tudo minusculo.
+            char type = char.Parse(data.Substring(data.Length - 1, 1));//Pegando ultima caracter.
+
+            Console.WriteLine(data);
+            Console.WriteLine(type);
         }
 
         static void Start(int time)
