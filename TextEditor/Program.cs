dfa8ad983c4c8a4eb9fe367df28283de33032bfd
Yan Wallas <yanwallas98@gmail.com>
2024-10-03 14:01:13 -0300

    Do/While

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace TextEditor
 {
@@ -6,7 +7,49 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("O - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir()
+        {
+
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+            Console.WriteLine("------------------------");
+            string text = "";
+
+            do
+            { // faça isso.
+                text += Console.ReadLine();
+                text += Environment.NewLine;//Obtém a string de nova linha definida para este ambiente.
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);// Enquanto o usuario nao aperta ESC
+
+            Console.Write(text);
+
+
         }
     }
 }
