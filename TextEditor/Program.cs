a7f4f9cfbd91c079b6122fd0b8975712c05befd7
Yan Wallas <yanwallas98@gmail.com>
2024-10-03 13:43:07 -0300

    TextEditor

---
+++ TextEditor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
