30e545925f521845167c610ed553e66fd7201436
Yan Wallas <yanwallas98@gmail.com>
2024-10-09 14:28:30 -0300

    Guids

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -6,7 +6,16 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var id = Guid.NewGuid();//criando um ID (números grande) aleatorio.
+            id.ToString();//tranformando em string
+
+            id = new Guid("82b0e170-573e-451b-b9af-4ea63bd78440");//jogando a strind dentro so id
+
+            //New Guid - a gente passa o que era criado. 
+            //id = new Guid();//Se eu nao passar nada dentro do New guid, gera tudo 0000000.
+
+            Console.WriteLine(id.ToString().Substring(0, 8));//pegando apenas os 8 primeiros numeros.
+
         }
     }
 }
