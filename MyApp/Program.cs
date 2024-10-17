41b4a84e8ccd33a757bf140548fc649949c13ce1
Yan Wallas <yanwallas98@gmail.com>
2024-10-17 14:41:52 -0300

    espaçamento

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,25 +2,25 @@
 
 namespace MyApp
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            SobreGuid();
+      SobreGuid();
 
-        }
-
-        static void SobreGuid()
-        {
-            var id = Guid.NewGuid();//criando um ID (números grande) aleatorio.
-            id.ToString();//tranformando em string
-
-            id = new Guid("82b0e170-573e-451b-b9af-4ea63bd78440");//jogando a strind dentro so id
-
-            //New Guid - a gente passa o que era criado. 
-            //id = new Guid();//Se eu nao passar nada dentro do New guid, gera tudo 0000000.
-
-            Console.WriteLine(id.ToString().Substring(0, 8));//pegando apenas os 8 primeiros numeros.
-        }
     }
+
+    static void SobreGuid()
+    {
+      var id = Guid.NewGuid();//criando um ID (números grande) aleatorio.
+      id.ToString();//tranformando em string
+
+      id = new Guid("82b0e170-573e-451b-b9af-4ea63bd78440");//jogando a strind dentro so id
+
+      //New Guid - a gente passa o que era criado. 
+      //id = new Guid();//Se eu nao passar nada dentro do New guid, gera tudo 0000000.
+
+      Console.WriteLine(id.ToString().Substring(0, 8));//pegando apenas os 8 primeiros numeros.
+    }
+  }
 }
