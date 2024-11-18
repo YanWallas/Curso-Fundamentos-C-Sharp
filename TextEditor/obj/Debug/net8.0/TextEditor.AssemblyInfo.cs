1c371fb77398d56e89f3bb541022355520fd009e
Yan Wallas <yanwallas98@gmail.com>
2024-11-18 12:14:53 -0300

    .

--- TextEditor/obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
+++ TextEditor/obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aba60a882a7827c8a0eedc33acee309a6e464f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e77c19521ff271f35a2d51e6f667412e8a0c92e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
